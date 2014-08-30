3b39147b20c62bc855e2e2bdecbb703c3be62302
xopherdeep <heylisten@xtiv.net>
2014-08-30 01:53:50 -0700

    updated xengine to redirect found files instead of funnel them.

--- xengine.php
+++ xengine.php
@@ -2,7 +2,7 @@
 	/**
 	 * Xengine Version 2.x
 	 * @author XopherDeeP <heylisten@xtiv.net>
-	 * @version v2.2.6
+	 * @version v2.2.7
 	**/
 	
 	/*
@@ -248,6 +248,8 @@
 					$_SESSION['output'] = 'json';
 					unset($p[$key]);
 
+				}else if($value == '.html'){
+					$_SESSION['output'] = 'html';
 				}
 			}
 
@@ -760,30 +762,107 @@
 			$this->initSmarty(array_merge($assign, $this->_SET));
 		}
 
+		private function mime_content_type($filename) { 
+	        $mime_types = array(
+
+	            'txt' => 'text/plain',
+	            'htm' => 'text/html',
+	            'html' => 'text/html',
+	            'php' => 'text/html',
+	            'css' => 'text/css',
+	            'js' => 'application/javascript',
+	            'json' => 'application/json',
+	            'xml' => 'application/xml',
+	            'swf' => 'application/x-shockwave-flash',
+	            'flv' => 'video/x-flv',
+
+	            // images
+	            'png' => 'image/png',
+	            'jpe' => 'image/jpeg',
+	            'jpeg' => 'image/jpeg',
+	            'jpg' => 'image/jpeg',
+	            'gif' => 'image/gif',
+	            'bmp' => 'image/bmp',
+	            'ico' => 'image/vnd.microsoft.icon',
+	            'tiff' => 'image/tiff',
+	            'tif' => 'image/tiff',
+	            'svg' => 'image/svg+xml',
+	            'svgz' => 'image/svg+xml',
+
+	            // archives
+	            'zip' => 'application/zip',
+	            'rar' => 'application/x-rar-compressed',
+	            'exe' => 'application/x-msdownload',
+	            'msi' => 'application/x-msdownload',
+	            'cab' => 'application/vnd.ms-cab-compressed',
+
+	            // audio/video
+	            'mp3' => 'audio/mpeg',
+	            'qt' => 'video/quicktime',
+	            'mov' => 'video/quicktime',
+
+	            // adobe
+	            'pdf' => 'application/pdf',
+	            'psd' => 'image/vnd.adobe.photoshop',
+	            'ai' => 'application/postscript',
+	            'eps' => 'application/postscript',
+	            'ps' => 'application/postscript',
+
+	            // ms office
+	            'doc' => 'application/msword',
+	            'rtf' => 'application/rtf',
+	            'xls' => 'application/vnd.ms-excel',
+	            'ppt' => 'application/vnd.ms-powerpoint',
+
+	            // open office
+	            'odt' => 'application/vnd.oasis.opendocument.text',
+	            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+	        );
+
+	        $ext = strtolower(array_pop(explode('.',$filename)));
+	        if (array_key_exists($ext, $mime_types)) {
+	            return $mime_types[$ext];
+	        }
+	        elseif (function_exists('finfo_open')) {
+	            $finfo = finfo_open(FILEINFO_MIME);
+	            $mimetype = finfo_file($finfo, $filename);
+	            finfo_close($finfo);
+	            return $mimetype;
+	        }
+	        else {
+	            return 'application/octet-stream';
+	        }
+	    }
+
 		private function lookForFile()
 		{
- 
-			 
 			if(isset($_SERVER['REDIRECT_URL'])){
 				$look = $_SERVER['REDIRECT_URL'];
-				//$look = str_replace($this->_SET['action'].'/', '', $look);
-				 
-			
+
 				$c = $this->_CFG;
-				$_dir = __DIR__.'/../../'.$c['suite'].'/';
+				$_dir = XPHP_DIR.'/';
+
+
 
 				foreach ($this->getXtras() as $key => $value) {
-					$file = $_dir.$value['class'].'/'.$look;
+					$file = $_dir.$value['class'].$look;
+					$file;
 					
 					if(file_exists($file)){
-						$imginfo = getimagesize($file);
-						header("Content-type: ".$imginfo['mime']);
-						readfile($file);
+						$type =  $this->mime_content_type($file);
+						ob_clean();
+						header("Content-type: $type");
+						// Actuall file exist in xtra .display it.
+						header("Location: /".str_replace(DOC_ROOT, '', $file));
+
 						exit;
 					}
 				}
-			}
+			} 
 		}
+
+
+
 
  		/**
 	     * initSmarty() includes the smarty files and configures the new class:
